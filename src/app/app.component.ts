8e3b5ca53c2ecce5d739c2ad25d84c4a30fe013d
JunyuWang <happystyle666@gmail.com>
2023-03-05 22:56:00 +0800

    補註解

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,7 @@ export class AppComponent implements OnInit {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     if (environment.prod) {
+      // 因目前 API 非 https，所以需要這段 CSP，假裝
       this.metaService.addTag({ httpEquiv: 'Content-Security-Policy', content: "upgrade-insecure-requests" });
     }
 
